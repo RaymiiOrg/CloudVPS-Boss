335bac54c489b45e97d4ddef29b2819864db80d1
Remy <relst@relst.nl>
2014-07-08 10:38:39 +0200

    Remove wrong line in License. Closes #9
    Former-commit-id: ce46b4a540ee3f37f0c510c5c8a1eab72dcd2c0d

--- swiftstats.sh
+++ swiftstats.sh
@@ -17,8 +17,6 @@
 # with this program; if not, write to the Free Software Foundation, Inc., 
 # 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 # 
-# CloudVPS, hereby disclaims all copyright interest in the program 
-# `SwiftBackup' written by Remy van Elst.
 
 set -o pipefail
 
